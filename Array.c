22e34528a7b062deee2b32b11f44af7210967bb1
Nickco Paul <nickcopaul@gmail.com>
2023-05-17 10:54:11 +0630

    DSA - Array

---
+++ Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//what is array?
+
+int main(){
+    //arrary is to collect the data
+    int arrName[8] = {10,11,12,13,14,15,16};
+
+    for (int i = 0; i < 8; i++)
+    {
+        printf("This arrary number is %d\n",arrName[i]);
+    }
+    
+    
+    return 0;
+}
