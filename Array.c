d1bcd9f26fe8c4a0c9bffcb66d21b901cd8195a2
Nickco Paul <nickcopaul@gmail.com>
2023-05-22 20:12:44 +0630

    Add array part 1 data structures

---
+++ Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+//what is array?
+
+int main(){
+    //arrary is to collect the data
+    int arrName[10] = {10,11,12,13,14,15,16};
+
+    for (int i = 0; i <10; i++)
+    {
+        printf("This arrary number is %d\n",arrName[i]);
+    }
+    
+    
+    return 0;
+}
